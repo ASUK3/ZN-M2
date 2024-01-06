a32111b4048789a5f487880d74f7cf8e7d47c0e2
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2024-01-07 04:39:40 +0800

    Update diy-part1.sh

--- custom/cmiot_ax18/plus/diy-part1.sh
+++ custom/cmiot_ax18/plus/diy-part1.sh
@@ -16,3 +16,15 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 修改时区 UTF-8
+sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
+
+# 默认ip 192.168.6.1
+sed -i 's/192.168.[0-9]\{1,3\}.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# 时区
+sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
