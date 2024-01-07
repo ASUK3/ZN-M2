82b75be8c28c0cb3146ad78a2511d7a99a08b418
ASUK3 <120798558+ASUK3@users.noreply.github.com>
2024-01-07 22:10:06 +0800

    Update diy-part1.sh

--- custom/cmiot_ax18/plus/diy-part1.sh
+++ custom/cmiot_ax18/plus/diy-part1.sh
@@ -16,6 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git packages https://git.openwrt.org/feed/packages' >>feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci' >>feeds.conf.default
+echo 'src-git extras https://github.com/openwrt-dev/extras-ipq60xx' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony' >>feeds.conf.default
+echo 'src-git freifunk https://github.com/freifunk/openwrt-packages' >>feeds.conf.default
 
 # 修改时区 UTF-8
 sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
@@ -28,3 +34,6 @@ sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/b
 sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
 sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
 sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+
+# 替换源 
+sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
